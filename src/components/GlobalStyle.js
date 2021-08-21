baab940ed84866741b6f31c547cc708c70c731db
anis <anis.cse5.bu@gmail.com>
2021-08-21 07:19:34 +0600

    7.styles

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -7,6 +7,7 @@ const GlobalStyle = createGlobalStyle`
 }
 body{
   background: #1b1b1b;
+  font-family: 'Inter', sans-serif;
 }
 
 button{
@@ -18,6 +19,8 @@ button{
   background: transparent;
   color: white;
   transition: all 0.5s ease;
+  font-family: 'Inter', sans-serif;
+
   &:hover{
     background-color: #23d997;
     color: white;
