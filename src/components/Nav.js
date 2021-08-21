76458539b306d86b2b1568e41e08fa48a5e814a8
anis <anis.cse5.bu@gmail.com>
2021-08-21 23:56:44 +0600

    8.commit

---
+++ src/components/Nav.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Nav = () => {
+  return (
+    <StyledNav>
+      <h1>
+        <a href='#'>Capture</a>
+      </h1>
+      <ul>
+        <li>
+          <a href='#'>1.About Us</a>
+        </li>
+        <li>
+          <a href='#'>2.Our Work</a>
+        </li>
+        <li>
+          <a href='#'>3.Contact Us</a>
+        </li>
+      </ul>
+    </StyledNav>
+  );
+};
+
+const StyledNav = styled.nav`
+  min-height: 10vh;
+  display: flex;
+  margin: auto;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 10rem;
+  a {
+    color: white;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    list-style: none;
+  }
+`;
+
+export default Nav;
