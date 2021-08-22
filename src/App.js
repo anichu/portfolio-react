204eaed6f8d7db94cbe52ba16a1672848357ed4b
anis <anis.cse5.bu@gmail.com>
2021-08-22 16:48:36 +0600

    9.

--- src/App.js
+++ src/App.js
@@ -5,12 +5,25 @@ import GlobalStyle from './components/GlobalStyle';
 // Import Pages
 import AboutUs from './pages/AboutUs';
 import Nav from './components/Nav';
+import OurWork from './pages/OurWork';
+import ContactUs from './pages/ContactUs';
+import { Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <div className='App'>
       <GlobalStyle />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route exact path='/'>
+          <AboutUs />
+        </Route>
+        <Route path='/work'>
+          <OurWork />
+        </Route>
+        <Route path='/contact-us'>
+          <ContactUs />
+        </Route>
+      </Switch>
     </div>
   );
 }
